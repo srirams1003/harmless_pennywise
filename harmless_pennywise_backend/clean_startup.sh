5d027222f3a2fd4548accfea00cada0576cb9350
srirams1003 <57574798+srirams1003@users.noreply.github.com>
2025-03-05 10:23:45 -0600

    Update clean_startup.sh

--- harmless_pennywise_backend/clean_startup.sh
+++ harmless_pennywise_backend/clean_startup.sh
@@ -1,6 +1,6 @@
 docker stop harmless_pennywise_db
 docker rm harmless_pennywise_db
 docker run --name harmless_pennywise_db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=harmless_pennywise -p 3307:3306 -d mysql:latest
-sleep 15
+sleep 25
 python database.py
 uvicorn main:app --reload
